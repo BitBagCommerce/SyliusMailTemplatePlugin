b1698a06e09b939061e75deb186db09c46c19157
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2022-01-27 15:31:29 +0100

    Add MailPreviewDataProvider spec

---
+++ spec/Provider/MailPreviewDataProviderSpec.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace spec\BitBag\SyliusMailTemplatePlugin\Provider;
+
+use BitBag\SyliusMailTemplatePlugin\MailPreviewData\MailPreviewDataInterface;
+use BitBag\SyliusMailTemplatePlugin\Provider\MailPreviewDataProvider;
+use PhpSpec\ObjectBehavior;
+use Webmozart\Assert\InvalidArgumentException;
+
+class MailPreviewDataProviderSpec extends ObjectBehavior
+{
+    public const FOO = 'foo';
+
+    function let(): void
+    {
+        $this->beConstructedWith([]);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(MailPreviewDataProvider::class);
+    }
+
+    function it_should_throw_exception_if_generic_mail_preview_data_doesnt_exist(): void
+    {
+        $this->shouldThrow(InvalidArgumentException::class)->during('get', [self::FOO]);
+    }
+
+    function it_should_return_generic_mail_preview_data_if_none_mail_preview_data_for_requested_type_exist(
+        MailPreviewDataInterface $genericMailPreviewData
+    ): void {
+        $availableMailPreviewData = [
+            MailPreviewDataProvider::GENERIC_PREVIEW_DATA_KEY => $genericMailPreviewData,
+        ];
+        $this->beConstructedWith($availableMailPreviewData);
+
+        $mailPreviewData = $this->get(self::FOO);
+
+        $mailPreviewData->shouldBe($genericMailPreviewData);
+    }
+
+    function it_should_throw_exception_if_matched_mail_preview_data_is_not_implementing_mail_preview_data_interface(): void
+    {
+        $availableMailPreviewData = [
+            self::FOO => new \stdClass(),
+        ];
+
+        $this->beConstructedWith($availableMailPreviewData);
+
+        $this->shouldThrow(InvalidArgumentException::class)->during('get', [self::FOO]);
+    }
+
+    function it_should_return_mail_preview_data_for_requested_type(
+        MailPreviewDataInterface $genericMailPreviewData,
+        MailPreviewDataInterface $fooMailPreviewData
+    ): void {
+        $availableMailPreviewData = [
+            MailPreviewDataProvider::GENERIC_PREVIEW_DATA_KEY => $genericMailPreviewData,
+            self::FOO => $fooMailPreviewData,
+        ];
+        $this->beConstructedWith($availableMailPreviewData);
+
+        $this->get(self::FOO)->shouldBe($fooMailPreviewData);
+    }
+}
