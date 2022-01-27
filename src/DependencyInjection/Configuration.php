64e92806265916f132d1a5e1c2a645a90ed52f13
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2022-01-27 16:24:20 +0100

    Replace parameters from config.yaml with Configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,13 +15,64 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 final class Configuration implements ConfigurationInterface
 {
+    public const TREE_NAME = 'bit_bag_sylius_mail_template';
+
+    public const TWIG = 'twig';
+
+    public const ALLOWED_FILTERS = 'allowed_filters';
+
+    public const ALLOWED_FUNCTIONS = 'allowed_functions';
+
+    public const ALLOWED_METHODS = 'allowed_methods';
+
+    public const ALLOWED_PROPERTIES = 'allowed_properties';
+
+    public const ALLOWED_TAGS = 'allowed_tags';
+
+    public const ALLOWED_METHODS_DEFAULT = ['*' => '*'];
+
     /**
      * @psalm-suppress UnusedVariable
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('bitbag_sylius_mail_template_plugin');
+        $treeBuilder = new TreeBuilder(self::TREE_NAME);
         $rootNode = $treeBuilder->getRootNode();
+
+        $rootNode
+            ->children()
+                ->arrayNode(self::TWIG)
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode(self::ALLOWED_FILTERS)
+                            ->useAttributeAsKey('name')
+                            ->defaultValue([])
+                            ->prototype('scalar')->end()
+                        ->end()
+                        ->arrayNode(self::ALLOWED_FUNCTIONS)
+                            ->useAttributeAsKey('name')
+                            ->defaultValue([])
+                            ->prototype('scalar')->end()
+                        ->end()
+                        ->arrayNode(self::ALLOWED_METHODS)
+                            ->useAttributeAsKey('name')
+                            ->defaultValue(self::ALLOWED_METHODS_DEFAULT)
+                            ->prototype('scalar')->end()
+                        ->end()
+                        ->arrayNode(self::ALLOWED_PROPERTIES)
+                            ->useAttributeAsKey('name')
+                            ->defaultValue([])
+                            ->prototype('scalar')->end()
+                        ->end()
+                        ->arrayNode(self::ALLOWED_TAGS)
+                            ->useAttributeAsKey('name')
+                            ->defaultValue([])
+                            ->prototype('scalar')->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
