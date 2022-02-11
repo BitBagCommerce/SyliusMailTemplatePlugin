2d8b01bae94180a8599042df6f5fdc81e7ffaab6
Norbert Glanc <norglanc@gmail.com>
2022-02-11 07:46:26 +0100

    Add specs for return used email types

--- spec/Provider/EmailCodesProviderSpec.php
+++ spec/Provider/EmailCodesProviderSpec.php
@@ -48,4 +48,36 @@ class EmailCodesProviderSpec extends ObjectBehavior
         $emailCodesWithLabels->shouldHaveKeyWithValue(self::ORDER_CONFIRMATION_LABEL, Emails::ORDER_CONFIRMATION);
         $emailCodesWithLabels->shouldHaveKeyWithValue(self::SHIPMENT_CONFIRMATION_LABEL, Emails::SHIPMENT_CONFIRMATION);
     }
+
+    function it_should_return_email_template_types(EmailTemplateRepositoryInterface $emailTemplateRepository): void
+    {
+        $emailTemplateRepository->getAllTypes()->willReturn([]);
+
+        $emailCodesWithLabels = $this->provideWithLabelsNotUsedTypes();
+
+        $emailCodesWithLabels->shouldBeArray();
+        $emailCodesWithLabels->shouldHaveKeyWithValue(self::CONTACT_REQUEST_LABEL, Emails::CONTACT_REQUEST);
+        $emailCodesWithLabels->shouldHaveKeyWithValue(self::ORDER_CONFIRMATION_LABEL, Emails::ORDER_CONFIRMATION);
+        $emailCodesWithLabels->shouldHaveKeyWithValue(self::SHIPMENT_CONFIRMATION_LABEL, Emails::SHIPMENT_CONFIRMATION);
+    }
+
+    function it_should_return_not_email_template_types(EmailTemplateRepositoryInterface $emailTemplateRepository): void
+    {
+        $emailTemplateRepository->getAllTypes()->willReturn([
+            [
+                'type' => 'contact_request'
+            ],
+            [
+                'type' => 'order_confirmation'
+            ],
+            [
+                'type' => 'shipment_confirmation'
+            ]
+        ]);
+
+        $emailCodesWithLabels = $this->provideWithLabelsNotUsedTypes();
+
+        $emailCodesWithLabels->shouldBeArray();
+        $emailCodesWithLabels->shouldBeEqualTo([]);
+    }
 }
