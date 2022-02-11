a3cda68ace8f2f854ec2989bd1afac768c0433f7
Norbert Glanc <norglanc@gmail.com>
2022-02-11 08:13:52 +0100

    Add integration test for used email types, add run PHPUnit in github workflows

---
+++ tests/Repository/EmailTemplateTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+ */
+
+declare(strict_types=1);
+
+namespace Tests\BitBag\SyliusMailTemplatePlugin\Repository;
+
+use PHPUnit\Framework\Assert;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class EmailTemplateTest extends WebTestCase
+{
+    private $emailTemplateRepository;
+
+    protected function setUp(): void
+    {
+        $this->emailTemplateRepository = self::getContainer()->get('bitbag_sylius_mail_template_plugin.custom_repository.email_template');
+    }
+
+    public function test_return_used_template_email_types(): void
+    {
+        $emailTemplateTypes = $this->emailTemplateRepository->getAllTypes();
+
+        Assert::assertIsArray($emailTemplateTypes, 'This is not an array.');
+
+        Assert::assertCount( 1, $emailTemplateTypes);
+
+        Assert::assertArrayHasKey('type', $emailTemplateTypes[0]);
+    }
+}
